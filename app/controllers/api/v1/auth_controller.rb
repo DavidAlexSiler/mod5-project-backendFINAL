4f9920bebae58724fbe416bb9d2b44da64ad7c06
github username <github email address>
2019-11-04 12:14:47 -0500

    message

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::AuthController < ApplicationController
         query_params = { 
             client_id: Rails.application.credentials[:spotify][:client_id],
             response_type: 'code',
-            redirect_uri: 'https://current-input-front.herokuapp.com/callback',
+            # redirect_uri: 'http://localhost:8888/callback',
+            redirect_uri: 'http://localhost:8888/callback',
             scope: "
             playlist-modify-public
             playlist-modify-private 
